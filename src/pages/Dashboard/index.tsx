ad6797c63fd795e86cad2e2d83b5af1c1c15af27
Ricardo Emerson <ricardo_emerson@yahoo.com.br>
2020-04-23 22:12:26 -0300

    Finalizada a listagem de transações e adição dos itens de menu.

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-
+import React, { useState, useEffect, useMemo } from 'react';
+import { parseISO, format } from 'date-fns';
 import income from '../../assets/income.svg';
 import outcome from '../../assets/outcome.svg';
 import total from '../../assets/total.svg';
@@ -9,6 +9,7 @@ import api from '../../services/api';
 import Header from '../../components/Header';
 
 import formatValue from '../../utils/formatValue';
+import formatDate from '../../utils/formatDate';
 
 import { Container, CardContainer, Card, TableContainer } from './styles';
 
@@ -30,12 +31,39 @@ interface Balance {
 }
 
 const Dashboard: React.FC = () => {
-  // const [transactions, setTransactions] = useState<Transaction[]>([]);
-  // const [balance, setBalance] = useState<Balance>({} as Balance);
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+  const [balance, setBalance] = useState<Balance>({} as Balance);
+
+  const formattedBalance = useMemo(() => ({
+    income: formatValue(+balance.income || 0),
+    outcome: formatValue(+balance.outcome || 0),
+    total: formatValue(+balance.total || 0),
+  }),
+  [balance]);
 
   useEffect(() => {
     async function loadTransactions(): Promise<void> {
-      // TODO
+      const response = await api.get<{
+        transactions: Transaction[];
+        balance: Balance;
+      }>('/transactions');
+
+      const {
+        transactions: transactionsData,
+        balance: balanceData,
+      } = response.data;
+
+      console.log('transactionsData: ', transactionsData);
+
+      const formattedTransactions = transactionsData.map((transaction: Transaction) => ({
+        ...transaction,
+        formattedValue: formatValue(transaction.value),
+        formattedDate: format(new Date(Date.parse(transaction.created_at.toString())), 'dd/MM/yyyy')
+      }))
+
+      setTransactions([...formattedTransactions]);
+
+      setBalance(balanceData);
     }
 
     loadTransactions();
@@ -51,21 +79,21 @@ const Dashboard: React.FC = () => {
               <p>Entradas</p>
               <img src={income} alt="Income" />
             </header>
-            <h1 data-testid="balance-income">R$ 5.000,00</h1>
+            <h1 data-testid="balance-income">{ formattedBalance.income }</h1>
           </Card>
           <Card>
             <header>
               <p>Saídas</p>
               <img src={outcome} alt="Outcome" />
             </header>
-            <h1 data-testid="balance-outcome">R$ 1.000,00</h1>
+            <h1 data-testid="balance-outcome">{ formattedBalance.outcome }</h1>
           </Card>
           <Card total>
             <header>
               <p>Total</p>
               <img src={total} alt="Total" />
             </header>
-            <h1 data-testid="balance-total">R$ 4000,00</h1>
+            <h1 data-testid="balance-total">{ formattedBalance.total }</h1>
           </Card>
         </CardContainer>
 
@@ -81,18 +109,14 @@ const Dashboard: React.FC = () => {
             </thead>
 
             <tbody>
-              <tr>
-                <td className="title">Computer</td>
-                <td className="income">R$ 5.000,00</td>
-                <td>Sell</td>
-                <td>20/04/2020</td>
-              </tr>
-              <tr>
-                <td className="title">Website Hosting</td>
-                <td className="outcome">- R$ 1.000,00</td>
-                <td>Hosting</td>
-                <td>19/04/2020</td>
-              </tr>
+              { transactions.map(transaction => (
+                <tr key={ transaction.id }>
+                  <td className="title">{ transaction.title }</td>
+                  <td className={ transaction.type }>{ transaction.formattedValue }</td>
+                  <td>{ transaction.category.title }</td>
+                  <td>{ transaction.formattedDate }</td>
+                </tr>
+              )) }
             </tbody>
           </table>
         </TableContainer>
